dff68c8384946c5e011de3043175ead8b85589e1
supaheckafresh <neilstrain@gmail.com>
2016-02-01 19:27:40 -0800

    Make previous changes in public files :/

--- public/javascripts/filters.js
+++ public/javascripts/filters.js
@@ -4,10 +4,10 @@
 
     app.filter('capitalize', function() {
         return function(input, scope) {
-            if (input != null)
+            if (input !== null)
                 input = input.toLowerCase();
             return input.substring(0,1).toUpperCase()+input.substring(1);
-        }
+        };
     });
 
 }());
